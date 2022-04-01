4d2892acf92a5015f927026f07ce640b654d6ebc
Patrick <patrick.b.saxton@gmail.com>
2022-04-01 10:42:41 -0500

    Link Change

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -5,42 +5,42 @@ import jwt_decode from "jwt-decode";
 import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 // Register User
-export const registerUser = (userData, history) => dispatch => {
-  axios
-    .post("/api/users/register", userData)
-    .then(res => history.push("/login"))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
-};
+export const registerUser = (userData, history) => (dispatch) => {
+    axios
+        .post('https://realtimeproductivity.herokuapp.com/api/users/register', userData)
+        .then((res) => history.push('/login'))
+        .catch((err) =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        )
+}
 
 // Login - get user token
-export const loginUser = userData => dispatch => {
-  axios
-    .post("/api/users/login", userData)
-    .then(res => {
-      // Save to localStorage
+export const loginUser = (userData) => (dispatch) => {
+    axios
+        .post('https://realtimeproductivity.herokuapp.com/api/users/login', userData)
+        .then((res) => {
+            // Save to localStorage
 
-      // Set token to localStorage
-      const { token } = res.data;
-      localStorage.setItem("jwtToken", token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get user data
-      const decoded = jwt_decode(token);
-      // Set current user
-      dispatch(setCurrentUser(decoded));
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
-};
+            // Set token to localStorage
+            const { token } = res.data
+            localStorage.setItem('jwtToken', token)
+            // Set token to Auth header
+            setAuthToken(token)
+            // Decode token to get user data
+            const decoded = jwt_decode(token)
+            // Set current user
+            dispatch(setCurrentUser(decoded))
+        })
+        .catch((err) =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        )
+}
 
 // Set logged in user
 export const setCurrentUser = decoded => {
