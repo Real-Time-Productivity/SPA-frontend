143a005a701b171ae931fc147eb84b3a52f094ef
Patrick <73594427+Watersmash3@users.noreply.github.com>
2022-04-03 18:23:49 -0500

    Change dashboard to Pomodoro Only

--- src/App.js
+++ src/App.js
@@ -12,12 +12,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
-import Profile from "./components/dashboard/Profile";
-import ShowTaskList from "./components/dashboard/Tasks/TaskList";
-import CreateTask from "./components/dashboard/Tasks/CreateTask";
-
 import "./App.css";
-import PomodoroComp from "./components/dashboard/Pomodoro";
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
@@ -49,10 +44,6 @@ class App extends Component {
                         <Route exact path="/login" component={Login} />
                         <Switch>
                             <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                            <PrivateRoute exact path="/profile" component={Profile} />
-                            <PrivateRoute exact path="/tasks" component={ShowTaskList} />
-                            <PrivateRoute exact path="/create-todo" component={CreateTask} />
-                            <PrivateRoute exact path="/pomodoro" component={PomodoroComp} />
                         </Switch>
                     </div>
                 </Router>
