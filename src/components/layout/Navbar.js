d31c8b41983b8d27612837badbbf53caa0da3c1d
Mackey <74989114+TheRam21@users.noreply.github.com>
2022-04-29 13:49:57 -0500

    Fixed Typos

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,28 +2,28 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <div className="nav-wrapper white">
-                        <Link
-                            to="/"
-                            style={{
-                                fontFamily: "monospace",
-                                display: "flex",
-                                flexDirection: "row",
-                            }}
-                            className="col s5 brand-logo center black-text Navbar-Header"
-                        >
-                            <i className="material-icons Navbar-Icon">rocket_launch</i>
-                            Real Time Productity
-                        </Link>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace",
+                display: "flex",
+                flexDirection: "row",
+              }}
+              className="col s5 brand-logo center black-text Navbar-Header"
+            >
+              <i className="material-icons Navbar-Icon">rocket_launch</i>
+              Real Time Productivity
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
--- src/components/private-layout/PFooter.js
+++ src/components/private-layout/PFooter.js
@@ -1,47 +1,50 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'
-import './PFooter.css'
+import { Link } from "react-router-dom";
+import "./PFooter.css";
 
 export default class PFooter extends Component {
-    render() {
-        return (
-            <footer
-                className='page-footer'
-                style={{
-                    position: 'fixed',
-                    bottom: '0px',
-                    width: '100%',
-                    height: '60px',
-                }}
-            >
-                <div
-                    className='container center col'
-                    style={{
-                        width: '100%',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                    }}
-                >
-                    <div
-                        style={{
-                            margin: '0px 20px 0px 20px',
-                        }}
-                    >
-                        Real Time Productity
-                    </div>
+  render() {
+    return (
+      <footer
+        className="page-footer"
+        style={{
+          position: "fixed",
+          bottom: "0px",
+          width: "100%",
+          height: "60px",
+        }}
+      >
+        <div
+          className="container center col"
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              margin: "0px 20px 0px 20px",
+            }}
+          >
+            Real Time Productivity
+          </div>
 
-                    <div
-                        style={{
-                            margin: '0px 20px 0px 20px',
-                        }}
-                    >
-                        <Link to='/tipsandtricks' className='brand-logo white-text Dashboard-Nav'>
-                            Tips and Tricks
-                        </Link>
-                    </div>
-                </div>
-            </footer>
-        )
-    }
+          <div
+            style={{
+              margin: "0px 20px 0px 20px",
+            }}
+          >
+            <Link
+              to="/tipsandtricks"
+              className="brand-logo white-text Dashboard-Nav"
+            >
+              Tips and Tricks
+            </Link>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
